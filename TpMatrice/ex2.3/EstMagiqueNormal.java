526ec249fb91b1dc33b8ac4a32e7fda1bcc60414
1X 2X 3X <1fois2fois3fois@gmail.com>
2025-03-28 15:53:54 +0100

    add ex2.3

---
+++ TpMatrice/ex2.3/EstMagiqueNormal.java
@@ -0,0 +1,122 @@
+public class EstMagiqueNormal{
+
+	public static int calculeSomme(int[][] m, int posLigne, int posColonne, int dirX, int dirY){
+		int somme;
+		int	i;
+
+		somme = 0;
+		i = 0;
+		while(i < m.length){ //debut a la position
+			somme = somme + m[posLigne][posColonne];
+			posLigne = posLigne + dirX; //X pour les lignes
+			posColonne = posColonne + dirY; //Y pour les colonnes
+			i++;
+		}
+		return somme;
+	}
+	public static boolean sommeIdentique(int[][] m){
+		int	somme1;
+		int	lignes;
+		int	colonnes;
+		int	i;
+
+		somme1 = calculeSomme(m,0,0,0,1);
+
+		//LIGNES
+		lignes = 0;
+		while(lignes < m.length){ //boucle pour parcour toute les lignes 
+			if(calculeSomme(m,lignes,0,0,1) != somme1){
+				return false;
+			}
+			lignes++;
+		}
+		
+		//COLOLNNES
+		colonnes = 0;
+		while(colonnes < m[0].length){
+			if(calculeSomme(m,0,colonnes,1,0)!= somme1){
+				return false;
+			}
+			colonnes++;
+		}
+
+		if(calculeSomme(m,0,0,1,1) != somme1){ // pas de boucle, il y'a que une seul diag (haut gauche -> bas droite)
+			return false;
+		}
+
+		if(calculeSomme(m,0,m.length -1,1,-1) != somme1){// haut droite -> bas gauche 
+			return false;
+		}
+
+		return true;
+
+	}
+	//AJOUT DE LA FONCITON ESTMAGIQUE A NOTRE CODE PRECEDENT
+	public static boolean estMagique(int[][] m){
+		if(m.length != m[0].length || m == null){
+			return false;
+		}
+		
+		int	i;
+		int	j;
+
+		i = 0;
+		while(i < m.length){
+			j = 0;
+			while(j < m[0].length){	
+				if(m[i][j] <= 0){
+					return false;
+				}
+				j++;
+			}
+			i++;
+		}
+
+		System.out.println("La matrice est un carré magique, ok pour la suite.");
+		return sommeIdentique(m);
+	}
+
+	public static boolean estMagiqueNormal(int[][] m){
+		if(m == null || m.length != m[0].length){
+			return false;
+		}
+
+		boolean[] verif;
+		int	i;
+		int	j;
+
+		verif = new boolean[m.length * m.length + 1];
+		i = 0;
+		while(i < m.length){
+			j = 0;
+			while(j <m[i].length){
+				if(m[i][j] < 1 || m[i][j] > (m.length * m.length)){
+					return false;
+				}
+				if (verif[m[i][j]] == true ){
+					return false;
+				}
+				else {
+					verif[m[i][j]] = true;
+				}
+				j++;
+			}
+			i++;
+		}
+		System.out.println("Est un carrée magique normal");
+		return estMagique(m);
+	}
+	public static void main(String[] args){
+		int[][] mat;
+		boolean	res;
+
+		mat = new int[][] {
+			{2,7,6},
+			{9,5,1},
+			{4,3,8}
+		};
+
+		res = estMagiqueNormal(mat);
+		System.out.println(res);
+	}
+}
